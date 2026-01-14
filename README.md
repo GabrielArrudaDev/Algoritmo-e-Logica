c8f163776aa1fa82dce4a9a47cf21aba527f847c
GabrielArrudaDev <93550330+GabrielArrudaDev@users.noreply.github.com>
2026-01-14 00:25:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Algoritmo-e-Logica
+Fundamentos de programação: Variáveis, Estruturas de Controle, Vetores, Matrizes e Funções. Práticas de lógica e algoritmos implementadas em JavaScript e Pseudocódigo.
---
+++ aula-1/aula-1.js
@@ -0,0 +1,33 @@
+let nota1 = 10;
+let nota2 = 7;
+let penalizacao = 3;
+
+let media = (nota1+nota2-penalizacao);
+let resultado = "A sua média é: " +media;
+
+console.log(resultado);
+
+// console.log((nota1+nota2)-penalizacao);
+
+let nome = "Gabriel";
+let sobrenome = "Arruda";
+let nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);
+
+console.log(nomeCompleto +". "+ resultado);
+
+// celsius > fahrenheit
+// multiplicamos a temperatura em ºC por 1,8 e somamos 32 ao resultado.
+
+let temperatura = 26;
+let multiplicacaoTemperatura = 1.8;
+let somaTemperatura = 32;
+let resultadoTemperatura = (temperatura * multiplicacaoTemperatura + somaTemperatura);
+
+console.log(resultadoTemperatura.toFixed(2));
+
+
+
+
+
