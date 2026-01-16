c9777f62b42ebdccc2a5dfc477c31a05ef377e4a
GabrielArrudaDev <93550330+GabrielArrudaDev@users.noreply.github.com>
2026-01-16 17:22:22 -0300

    Iniciando com function

---
+++ secao-4/aula-1.js
@@ -0,0 +1,15 @@
+ //Aprendendo sobre funções
+
+ function mostrarNome() {
+    console.log("Gabriel");
+ }
+
+ mostrarNome();
+
+ function gerarNumeroAleatorio() {
+    return Math.random();
+}
+
+ let numeroGerado = gerarNumeroAleatorio();
+ console.log(numeroGerado);
+ 
